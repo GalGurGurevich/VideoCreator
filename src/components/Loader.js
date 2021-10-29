67dfd9485434d9a238e54ab7cabbcda1a40bd923
Gal Gur <gal.gurevich@gmail.com>
2021-10-29 17:42:24 +0300

    added animated loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Loader.css'
+import AnimatedLoader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export default function Loader({loadingTxt}) {
 
     const txt = loadingTxt ? loadingTxt : "Loading, Please wait...";
 
     return (
-        <div className="loader-container d-flex align-items-center text-center font-weight-bold">{txt}</div>
+        <div className="loader-container justify-contnet-center d-flex flex-column align-items-center text-center font-weight-bold">
+            <AnimatedLoader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+            />
+        {txt}
+        </div>
     )
 }
